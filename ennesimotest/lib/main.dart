2c8cd81fe0a9c6819c06266f37fe136ea3287422
Davidtozz <34129822+Davidtozz@users.noreply.github.com>
2022-04-13 22:01:28 +0200

    Text() widget now updates itself periodically

--- ennesimotest/lib/main.dart
+++ ennesimotest/lib/main.dart
@@ -1,33 +1,36 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appTitle = 'ListGenerator';
- 
+
     return MaterialApp(
-     
       home: Scaffold(
         appBar: AppBar(
           foregroundColor: Color.fromARGB(255, 32, 75, 132),
-           title: Text(appTitle),
-            
-           centerTitle: true,
-           backgroundColor: Color.fromARGB(255, 11, 161, 242),
+          title: Text(appTitle),
+          
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 11, 161, 242),
         ),
-        body: Column(children: const[
-          Expanded(
-              flex: 1, 
-              child:  SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child:   ReadTextFile(),
-              )
-            ),
-          ], 
-        )  , //child: 
+        body: Column(
+          children: const [
+            Expanded(
+                flex: 1,
+                child: SingleChildScrollView(
+                  
+                  scrollDirection: Axis.vertical,
+                  child: ReadTextFile(),
+                )),
+          ],
+        ),
       ),
     );
   }
@@ -35,30 +38,60 @@ class MyApp extends StatelessWidget {
 
 class ReadTextFile extends StatefulWidget {
   const ReadTextFile({Key? key}) : super(key: key);
- 
+
   @override
   _ReadTextFileState createState() => _ReadTextFileState();
 }
- 
+
 class _ReadTextFileState extends State<ReadTextFile> {
   String dataFromFile = "";
- 
+  static List<String> listOne =      []; //contains all jobs.txt content, from top to bottom
+  
+
   Future<void> readText() async {
     final String response = await rootBundle.loadString('assets/jobs.txt');
+    listOne = response.split(','); 
+     int randomIndex = Random().nextInt(listOne.length);
     setState(() {
-      dataFromFile = response;
+   
+      if (listOne.length > 2) {
+        dataFromFile += listOne[Random().nextInt(listOne.length - 1)];
+      } else if (listOne.length == 1) {
+        dataFromFile += listOne.first;
+      }
     });
+  
+  }
+
+// //-------stackoverflow answer, try 1--------------------
+//   @override
+
+  void initData() async {
+    await readText();
   }
 
   @override
-  Widget build(BuildContext context) {
-    readText();
-    return Container(
-
-      child:Text(dataFromFile)
-      // child: Center(
-      //   child:Text(dataFromFile)
-      //   )
-    );
+  void initState() {
+    super.initState();
+    initData();
+    Timer.periodic(Duration(milliseconds: 300), ((timer) => readText()) );
   }
-}
+
+@override
+  Widget build(BuildContext context) {
+    // readText();
+    // showRandomJob();
+    return Container(child: Center(child: Text(dataFromFile, style: TextStyle(fontSize: 50, backgroundColor: Color.fromARGB(255, 255, 255, 2) ),)));
+  }
+  //  @override
+  // void initState() {
+  //   super.initState();
+  //   // change text after 1 sec
+  //   Timer(Duration(seconds: 5), () async {
+  //     await readText();
+  //   });
+  }
+  
+// // ----------------------------------------------------
+
+  
